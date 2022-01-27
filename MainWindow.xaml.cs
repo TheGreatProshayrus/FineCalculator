afe7dbf9657d6418759e593656646e5cdf0ffd29
Proshayrus <gerlynd.prokhor@gmail.com>
2022-01-27 16:10:39 +0200

    Days calculating

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace FineCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string Start_Date;
+        string End_Date;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public double GetDays(string date1, string date2)
+        {
+            DateTime start = DateTime.Parse(date1);
+            DateTime end = DateTime.Parse(date2);
+
+            return (end - start).TotalDays;
+        }
+        public void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Start_Date = StartData.SelectedDate.ToString();
+
+        }
+
+        public void DatePicker_SelectedDateChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            End_Date = EndData.SelectedDate.ToString();
+       
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           double result = GetDays(Start_Date, End_Date);
+            MessageBox.Show(result.ToString());
         }
     }
 }
